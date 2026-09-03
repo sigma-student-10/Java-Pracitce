4887bdc6af71a6b3679044aec121dbf78b5d6268
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-09-03 17:11:49 +0600

    add new code

---
+++ Java-Basics/program1.java
@@ -0,0 +1,13 @@
+// Product a & b 
+
+import java.util.*;
+public class program1 {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product = a * b;
+        System.out.println(product);
+    }
+}
