bdcc84c45dfebb25e77ab83a71d28f9f6d001898
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-09-03 17:28:17 +0600

    add new code

---
+++ Java-Basics/program2.java
@@ -0,0 +1,12 @@
+// area of circle
+
+import java.util.*;
+public class program2 {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        float rad = sc.nextFloat();
+        float area = 3.14f * rad * rad;
+        System.out.println(area);
+
+    }
+}
