9128de2b1583483f7b94919f8720a78dd5a7a6f2
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-09-04 19:51:59 +0600

    add new code

--- Java-Basics/program3.java
+++
@@ -1,9 +0,0 @@
- public class program3 {
-    public static void main (String args[]) {
-        char ch = 'a' ;
-        int number = ch;
-        System.out.println(number);
-    }
- }
-    
-
---
+++ conditional-statement/program1.java
@@ -0,0 +1,15 @@
+public class program1 {
+    public static void main (String args[]) {
+        int age = 22;
+        if(age >= 18) {
+            System.out.println("adult: drive, vote");
+        }
+
+        if(age > 13 && age < 18) {
+            System.out.println("teenager: can't drive or vote");
+        }
+        else {
+            System.out.println("not adult");
+        }
+    }
+}
