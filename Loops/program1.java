b904750246ede4bba7e2845db58da626ab8cbc6c
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-09-08 17:32:04 +0600

    add new code

---
+++ Loops/program1.java
@@ -0,0 +1,13 @@
+// while loop
+
+import java.util.*;
+public class program1 {
+    public static void main(String args[]) {
+        int counter = 0;
+        while(counter < 20 ) {
+            System.out.println("Hello World");
+            counter++;
+        }
+        System.out.println("printer HW 100x");
+    }
+}
