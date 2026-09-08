f75a68e2b2fe30e6507cb699847ebb8f8e0e3fda
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-09-08 17:03:24 +0600

    add new code

---
+++ conditional-statement/program4.java
@@ -0,0 +1,16 @@
+// else if staement example
+
+import java.util.*;
+public class program4 {
+    public static void main (String args[]) {
+        int age = 13;
+
+        if(age >= 18 ) {
+            System.out.println("adult");
+        } else if((age >= 13) && (age < 18 ) ) {
+            System.out.println("teenager");
+        } else {
+            System.out.println("child");
+        }
+    }
+}
