b1762d4abda764738540acf377392d98b9ceab5c
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-09-08 17:46:39 +0600

    add new code

---
+++ Loops/program2.java
@@ -0,0 +1,12 @@
+// while loops
+import java.util.*;
+public class program2 {
+    public static void main(String args[]) {
+        int counter = 1;
+        while(counter <= 10) {
+            System.out.print(counter + " ");
+            counter++;
+        }
+        System.out.println();
+    }
+}
